b58fff8093f83d61b3a03804dad4f544b038a15d
Perly Gustafsson <contact@daperley.com>
2016-12-10 01:13:30 +0100

    Sleep time
    Started with Chapter 6, though it's time for sleep, to be continued

---
+++ WorkingWithVisualStudio/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace WorkingWithVisualStudio
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseBrowserLink();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
