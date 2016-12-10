3fcd7ce5a95595c33117056686e8dec15ded707d
Perly Gustafsson <contact@daperley.com>
2016-12-10 21:47:14 +0100

    SportsStore - Chap8
    -Without Unit Test since the project.json deliver so many errors it's to
    time consuming to look at right now, I'll go back to take a look at that
    in the future
    
    -Also added a 0 in the end of all pricings sincce the swedish currency
    made everything almost for free otherwise, TODO: Add an currency
    converter

---
+++ SportsStore/Models/FakeProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace SportsStore.Models
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        public IEnumerable<Product> Products => new List<Product> {
+             new Product { Name = "Football", Price = 250 },
+             new Product { Name = "Surf board", Price = 1790 },
+             new Product { Name = "Running shoes", Price = 950 }
+         };
+    }
+}
